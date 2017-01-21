0380039d15ccbd0bf2a1db3ce068cf665e7d5206
Frank S. Thomas <frank@timepit.eu>
2017-01-21 08:44:49 +0100

    Remove Blaze object

--- modules/server/src/main/scala/funstack/FunApp.scala
+++ modules/server/src/main/scala/funstack/FunApp.scala
@@ -1,10 +1,17 @@
 package funstack
 
+import eu.timepit.refined.auto._
+import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.{Server, ServerApp}
 import scala.collection.immutable.List
 import scalaz.concurrent.Task
 
 object FunApp extends ServerApp {
   override def server(args: List[String]): Task[Server] =
-    Blaze.server
+    AppConf.loadConf.flatMap { conf =>
+      BlazeBuilder
+        .bindHttp(conf.httpPort, conf.httpHost)
+        .mountService(Service.route)
+        .start
+    }
 }
